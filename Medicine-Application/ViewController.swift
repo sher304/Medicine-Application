e3e80c3d0516f42444420419455f685a01190d0e
sher304 <esherovshermat@gmail.com>
2022-11-21 16:55:42 +0100

    :tada: Start Project

---
+++ Medicine-Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Medicine-Application
+//
+//  Created by Шермат Эшеров on 21/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Medicine-Application/Pods-Medicine-Application-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Medicine_Application : NSObject
+@end
+@implementation PodsDummy_Pods_Medicine_Application
+@end
